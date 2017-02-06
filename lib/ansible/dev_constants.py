1aeb74ede3aeb52cbce33d41672ff0b158f0427b
Jeremy Gurr <jeremy.gurr@nextiva.com>
2017-02-06 11:32:56 -0700

    Set debug default to proper value

--- lib/ansible/dev_constants.py
+++ lib/ansible/dev_constants.py
@@ -1,4 +1,4 @@
 # Contains constants for controlling debugging processes strictly for debugging, and should be off/false for production use.
 
-enable_debug_stepping = True
+enable_debug_stepping = False
 
